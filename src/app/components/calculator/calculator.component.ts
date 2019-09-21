c0fde4f0fcc362dad918a34ff3af943cde539b4a
EduardoMay <eduardo_may@outlook.com>
2019-09-21 13:24:30 -0500

    Mejorando vista y funcionalidad

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -8,7 +8,7 @@ import { Calculator } from 'src/app/interfaces/calculator';
 })
 export class CalculatorComponent implements OnInit {
 
-  public formValues: Calculator = {};
+  public formValues: Calculator = {totalPrice: 0, change: 0};
   public priceFood: number;
 
   constructor() { }
@@ -18,12 +18,14 @@ export class CalculatorComponent implements OnInit {
 
   // Calculator
   public calculator() {
-    if (this.formValues.cantSoda === 0 || this.formValues.cantSoda === undefined) {
-      this.formValues.totalPrice = this.priceFood * this.formValues.cantOrderFood;
-    } else if (this.formValues.cantSoda > 0) {
-      const priceTotalSoda = 5 * this.formValues.cantSoda;
+    if (this.formValues.cantOrderFood > 0) {
+      if (this.formValues.cantSoda === 0 || this.formValues.cantSoda === undefined) {
+        this.formValues.totalPrice = this.priceFood * this.formValues.cantOrderFood;
+      } else if (this.formValues.cantSoda > 0) {
+        const priceTotalSoda = 5 * this.formValues.cantSoda;
 
-      this.formValues.totalPrice = (this.priceFood * this.formValues.cantOrderFood) + priceTotalSoda;
+        this.formValues.totalPrice = (this.priceFood * this.formValues.cantOrderFood) + priceTotalSoda;
+      }
     }
   }
 
@@ -34,11 +36,15 @@ export class CalculatorComponent implements OnInit {
 
   // clear form
   public clear() {
-    this.formValues = {};
+    this.formValues = {change: 0};
+    this.formValues.totalPrice = 0;
+
+    console.log(this.formValues);
   }
 
   public savePriceFood(e) {
     this.priceFood = e.target.value;
+    this.saveTotalPrice();
   }
 
   public saveCantOrderFood(e) {
@@ -52,12 +58,14 @@ export class CalculatorComponent implements OnInit {
   }
 
   public saveTotalPrice() {
-    if (this.formValues.cantSoda === 0 || this.formValues.cantSoda === undefined) {
-      this.formValues.totalPrice = this.priceFood * this.formValues.cantOrderFood;
-    } else if (this.formValues.cantSoda > 0) {
-      const priceTotalSoda = 5 * this.formValues.cantSoda;
+    if (this.formValues.cantOrderFood > 0) {
+      if (this.formValues.cantSoda === 0 || this.formValues.cantSoda === undefined) {
+        this.formValues.totalPrice = this.priceFood * this.formValues.cantOrderFood;
+      } else if (this.formValues.cantSoda > 0) {
+        const priceTotalSoda = 5 * this.formValues.cantSoda;
 
-      this.formValues.totalPrice = (this.priceFood * this.formValues.cantOrderFood) + priceTotalSoda;
+        this.formValues.totalPrice = (this.priceFood * this.formValues.cantOrderFood) + priceTotalSoda;
+      }
     }
   }
 
